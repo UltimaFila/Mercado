858d77dc8ffe6844b6387fe82b1fd231fea2e8d2
Clarandom <clarasubiron@gmail.com>
2015-04-10 18:14:09 +0200

    Ahora zi

--- Mercancia.java
+++ Mercancia.java
@@ -1,4 +1,39 @@
-
 public class Mercancia {
+
+	String tipo;
+	float kgs;
+	float preciokg;
+
+	public Mercancia(String tipo_nuevo) {
+		this.tipo = tipo_nuevo;
+
+	}
+
+	public Mercancia(String tipo_nuevo, float kgs_nuevo, float preciokg_nuevo) {
+		this.tipo = tipo_nuevo;
+		this.kgs = kgs_nuevo;
+		this.preciokg = preciokg_nuevo;
+
+	}
+
+	public float getKgs() {
+		return (kgs);
+	}
+
+	public void setKgs(float kgs_nuevo) {
+		this.kgs = kgs_nuevo;
+	}
+
+	public float getPreciokg() {
+		return (preciokg);
+	}
+
+	public void setPreciokg(float preciokg_nuevo) {
+		this.preciokg = preciokg_nuevo;
+	}
+
+	public String listadoMercancia() {
+		return (this.tipo + " " + this.kgs + " " + this.preciokg + "");
+	}
 
 }
